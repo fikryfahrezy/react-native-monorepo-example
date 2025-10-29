3b81500dc9062bb03016bbfe49896b375d3d9ed6
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-10-29 13:14:25 +0700

    Setup project

---
+++ apps/example/metro.config.js
@@ -0,0 +1,25 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+
+// Find the project root (monorepo root)
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {
+  projectRoot,
+  watchFolders: [monorepoRoot],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(monorepoRoot, 'node_modules'),
+    ],
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
