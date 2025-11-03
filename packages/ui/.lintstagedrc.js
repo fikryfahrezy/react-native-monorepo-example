7aed592866658d33b2f228f2a2d1b5ac07f201df
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-11-03 14:42:34 +0700

    Remove Apps and Packages from react-native-cli

--- packages/ui/.lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-import baseConfig from "../../.lintstagedrc.js";
-
-export default {
-  ...baseConfig,
-  "*.{js,jsx,ts,tsx}": ["eslint", "prettier --write"],
-};
--- packages/ui/src/status-bar.tsx
+++
@@ -1,6 +0,0 @@
-import { StatusBar as RNStatusBar } from "react-native";
-import React from "react";
-
-export const StatusBar = (props: React.ComponentProps<typeof RNStatusBar>) => {
-  return <RNStatusBar {...props} />;
-};
